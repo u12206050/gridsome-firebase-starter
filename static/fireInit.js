50505a809da9159e1d0fa1fa60047ee6dd09de0e
Gerard Lamusse <>
2019-05-03 20:17:30 +0200

    On window load

--- static/fireInit.js
+++ static/fireInit.js
@@ -1,2 +1,4 @@
-console.log('%c Firebase loaded...', 'background: #ffcb2c; color: #f5820b');
-window.dispatchEvent(new Event('firebase-loaded'))
+window.addEventListener('load', function() {
+  console.log('%c Firebase loaded...', 'background: #ffcb2c; color: #f5820b');
+  window.dispatchEvent(new Event('firebase-loaded'));
+})
