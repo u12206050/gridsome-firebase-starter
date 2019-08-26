761a0db6a506c38875aec04c1a136a6d99bf4866
Rory Silva <typerory@gmail.com>
2019-08-26 12:29:41 -0400

    Lazy load Click component

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,6 +2,11 @@
   <main>
     <h1>About us</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+
+    <LazyHydrate when-idle>
+      <Clicks />
+    </LazyHydrate>
+
   </main>
 </template>
 
@@ -9,6 +14,9 @@
 export default {
   metaInfo: {
     title: 'About us'
+  },
+  components: {
+    Clicks: () => import('~/singles/Clicks.vue')
   }
 }
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -59,17 +59,9 @@
       </div>
     </section>
 
-    <section class="font-sans text-center py-8 px-4 lg:px-0 bg-gray-100">
-      <div class="container flex flex-col sm:flex-row bg-white max-w-xl m-auto shadow">
-        <div class="w-full sm:w-3/4 px-6 pt-6 text-left flex flex-col justify-center">
-          <h2 class="capitalize font-medium">Ready for an awesome Firebase & Gridsome powered app!</h2>
-          <p class="text-gray-900 mt-2 mb-6">You have clicked <strong v-text="clicks"></strong> times</p>
-        </div>
-        <div class="w-full sm:w-1/4 flex justify-start sm:justify-center items-center pl-6 pb-6 sm:pl-0 sm:pb-0">
-          <button class=" bg-black hover:bg-gray-900 text-white hover:text-white py-3 px-6 uppercase text-xs tracking-wide">Learn More</button>
-        </div>
-      </div>
-    </section>
+    <LazyHydrate when-idle>
+      <Clicks />
+    </LazyHydrate>
 
     <section class="font-sans container m-auto text-center py-8">
       <label for="tagline" class="uppercase tracking-wide font-bold text-gray-900">Popular Topics</label>
@@ -132,9 +124,6 @@ export default {
     topics() {
       return this.$page.allFireTopics.edges && this.$page.allFireTopics.edges.map(e => e.node)
     },
-    clicks() {
-      return this.$store.clicks
-    },
     user() {
       /**
        * This will get updated because
@@ -146,6 +135,9 @@ export default {
       return this.$store.documents
     }
   },
+  components: {
+    Clicks: () => import('~/singles/Clicks.vue')
+  }
 }
 </script>
 
